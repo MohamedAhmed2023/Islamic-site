7db94c93a4c43fc03e9cd8ecf48669d2a5f1b7a9
Mohamed Ahmed <112361025+MohamedAhmed2023@users.noreply.github.com>
2023-04-26 23:53:27 +0200

    Add files via upload

---
+++ hadith/hadith.js
@@ -0,0 +1,26 @@
+let hadithContainer = document.querySelector(".hadithContainer"),
+  next = document.querySelector(".buttons .next"),
+  prev = document.querySelector(".buttons .prev"),
+  number = document.querySelector(".buttons .number");
+let hadithIndex = 0;
+HadithChanger();
+function HadithChanger() {
+  fetch("https://api.hadith.gading.dev/books/bukhari?range=300-500")
+    .then((response) => response.json())
+    .then((data) => {
+      let Hadiths = data.data.hadiths;
+      changeHadith();
+      next.addEventListener("click", () => {
+        hadithIndex == 299 ? (hadithIndex = 0) : hadithIndex++;
+        changeHadith();
+      });
+      prev.addEventListener("click", () => {
+        hadithIndex == 0 ? (hadithIndex = 299) : hadithIndex--;
+        changeHadith();
+      });
+      function changeHadith() {
+        hadithContainer.innerText = Hadiths[hadithIndex].arab;
+        number.innerText = `300  -  ${hadithIndex + 1}`;
+      }
+    });
+}
